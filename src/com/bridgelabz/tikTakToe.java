a674401afee64cfc9eb882c820bc367ec9e91dc7
Supriya Bhurle <supriyabhurle28@gmail.com>
2022-12-02 13:38:30 +0530

    board added

--- src/com/bridgelabz/tikTakToe.java
+++ src/com/bridgelabz/tikTakToe.java
@@ -3,5 +3,17 @@ package com.bridgelabz;
 public class tikTakToe {
     public static void main(String[] args) {
         System.out.println("Welcome to tik tak toe game");
+
+        char[][] gameBoard = {{'_', '|', '_', '|', '_'}, {'_', '|', '_', '|', '_'}, {'_', '|', '_', '|', '_'}};
+        printBOARD(gameBoard);
+    }
+
+    public static void printBOARD(char [][] gameBoard){
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard[i].length; j++) {
+                System.out.print(gameBoard[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
